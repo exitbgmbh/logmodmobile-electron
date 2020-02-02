fc5f0810765ed145eb4c3d783af99203f0c6701a
roman <roman.robel@exitb.de>
2020-02-02 20:44:06 +0100

    testing autoupdater

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const os = require('os');
 const version = require(__dirname + '/../package').version;
+const { autoUpdater } = require("electron-updater");
 
 const index = require('electron');
 const { ipcMain } = require('electron');
@@ -15,11 +16,21 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 let mainWindow;
 function showWindow() {
+    autoUpdater.checkForUpdatesAndNotify();
+    autoUpdater.on('download-progress', (progressObj) => {
+        let log_message = "Download speed: " + progressObj.bytesPerSecond;
+        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+        console.log(log_message);
+    });
+    autoUpdater.on('update-downloaded', (info) => {
+        console.log('Update downloaded');
+    });
+
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 1280,
         height: 800,
-        show: false,
         icon: path.join(__dirname, '/../static/assets/logmodmobile.png'),
         webPreferences: {
             nodeIntegration: true,
@@ -32,7 +43,6 @@ function showWindow() {
     mainWindow.setMenu(null);
     mainWindow.loadURL(startUrl).then(() => {
         mainWindow.setTitle(mainWindow.getTitle() + ' • Client %s'.replace('%s', version));
-        mainWindow.show();
     });
 
     if (isDevelopment) {
