37e5664e9b33eaa26b6ab5f4b5391351842b7078
roman <roman.robel@exitb.de>
2020-11-09 12:27:11 +0100

    use logfile for output

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require('electron').app;
+const log = require('electron-log');
+log.transports.file.maxSize = 10485760;
+
 if (process.env.NODE_ENV !== 'development') {
+    console.log = log.log;
     const { checkConfig } = require('./setupConfig');
     checkConfig(app);
 }
