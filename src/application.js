cee18430dedd4ba612416261449c8c195c062e88
roman <roman.robel@exitb.de>
2025-05-25 14:49:50 +0200

    shipmentPackageLabel raw print

--- src/application.js
+++ src/application.js
@@ -26,7 +26,6 @@ const restSrvInstance = require('./rest');
 const {nanoid} = require("nanoid");
 const {loadPlugins} = require("../pluginLoader");
 const LocalStorage = require('node-localstorage').LocalStorage;
-const pcsclite = require('pcsclite')
 
 /**
  * the application main window instance
@@ -329,48 +328,6 @@ const bootApplication = () => {
     instantiateApplicationWindow();
     logInfo('application', 'bootApplication', 'end');
 };
-
-const initRFIDReader = () => {
-    const pcsc = pcsclite()
-    pcsc.on('reader', (reader) => {
-        logInfo('application', 'initRFIDReader', `connected to reader ${reader.name}`)
-        reader.on('status', (status) => {
-            if ((status.state & reader.SCARD_STATE_PRESENT)) {
-                console.log('Card inserted');
-
-                reader.connect({ share_mode : reader.SCARD_SHARE_SHARED }, function(err, protocol) {
-                    if (err) {
-                        console.error(err);
-                        return;
-                    }
-                    console.log('Protocol:', protocol);
-
-                    // APDU-Befehl zum UID-Auslesen
-                    const GET_UID = Buffer.from([0xFF, 0xCA, 0x00, 0x00, 0x00]);
-
-                    reader.transmit(GET_UID, 40, protocol, function(err, data) {
-                        if (err) {
-                            console.error('Transmit error:', err);
-                            return;
-                        }
-                        // UID + Statuswort (letzte 2 Bytes)
-                        const uid = data.slice(0, -2);
-                        const sw1 = data[data.length - 2];
-                        const sw2 = data[data.length - 1];
-
-                        console.log('UID:', uid.toString('hex').toUpperCase());
-                        console.log(`Status: ${sw1.toString(16)} ${sw2.toString(16)}`);
-
-                        reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {
-                            if (err) console.error(err);
-                            else console.log('Disconnected');
-                        });
-                    });
-                });
-            }
-        })
-    })
-}
 
 const showChangeLog = (force = false) => {
     let localStorage = new LocalStorage(path.join(app.getPath('userData'), 'storage.data'));
