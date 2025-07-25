380c43b92479d2a7d42abe14428ebc13be46a399
roman <roman.robel@exitb.de>
2025-07-26 00:27:05 +0200

    remove stuff

--- src/application.js
+++ src/application.js
@@ -49,7 +49,6 @@ const windowOnLoadCompleted = () => {
     if (windowTitle.indexOf('Client') === -1) {
         windowInstance.setTitle(windowInstance.getTitle() + ' • Client %s'.replace('%s', version));
     }
-
     logDebug('application', 'windowOnLoadCompleted', 'done');
 };
 
