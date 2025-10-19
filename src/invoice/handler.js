27a5d250e075686b0365c26bc0ec66800b13670d
roman <roman.robel@exitb.de>
2025-10-19 14:52:36 +0200

    suppress invoice printing

--- src/invoice/handler.js
+++ src/invoice/handler.js
@@ -14,39 +14,68 @@ class InvoiceHandler {
         eventEmitter.on('pickBoxReady', this.requestInvoiceForPickBox);
     };
 
-      requestInvoiceForPickBox = (requestData) => {
+    requestInvoiceForPickBox = (requestData) => {
         const {pickBoxIdent, force: forceInvoice = false} = requestData;
-        let messageTemplate = {event: null, type: 'pickBox', receiverUserId: 0, logModIdent: null, data: {pickBoxIdent: pickBoxIdent, invoiceNumber: null}};
+        let messageTemplate = {
+            event: null,
+            type: 'pickBox',
+            receiverUserId: 0,
+            logModIdent: null,
+            data: {pickBoxIdent: pickBoxIdent, invoiceNumber: null}
+        };
 
         restClientInstance
-              .requestInvoice(pickBoxIdent, forceInvoice)
-              .then((res) => {
+            .requestInvoice(pickBoxIdent, forceInvoice)
+            .then((res) => {
                 if (!res.success) {
-                  throw new Error('request not successful');
-              }
+                    console.log('invoiceHandler::requestInvoiceForPickBox::request invoice failed', res);
+                    throw new Error('request not successful');
+                }
 
-              const { response } = res;
-              let event = 'pickBoxInvoiceSuccess';
-              // event for lmm - toggle mask
-              eventEmitter.emit(event, {...messageTemplate, event: event, data: {...messageTemplate.data, invoiceNumber: response.invoiceNumber, shippingRequestNumber: response.shippingRequestNumber}});
+                const {response} = res;
+                const disableInvoicePrinting = config.has('printing.disablePrintingOnCreation') && config.get('printing.disablePrintingOnCreation');
 
-              if (config.has('printing.requestInvoiceDocumentsMerged') && config.get('printing.requestInvoiceDocumentsMerged') === true) {
-                  if (response.invoiceNumber === 'NO_INVOICE') {
-                      eventEmitter.emit('requestDocuments', {documentType: 'allDocs', invoiceNumber: response.shippingRequestNumber});
-                  } else {
-                      eventEmitter.emit('requestDocuments', {documentType: 'allDocs', invoiceNumber: response.invoiceNumber});
-                  }
-              } else {
-                  eventEmitter.emit('invoiceCreationSuccess', response);
-              }
-          })
-          .catch((res) => {
-            console.log(res);
+                let event = 'pickBoxInvoiceSuccess';
+                // event for lmm - toggle mask
+                eventEmitter.emit(event, {
+                    ...messageTemplate,
+                    event: event,
+                    data: {
+                        ...messageTemplate.data,
+                        invoiceNumber: response.invoiceNumber,
+                        shippingRequestNumber: response.shippingRequestNumber,
+                        printingIsSkipped: disableInvoicePrinting
+                    }
+                });
 
-            let event = 'pickBoxInvoiceFailed';
-            eventEmitter.emit(event, {...messageTemplate, event: event});
-          });
-      };
+                if (disableInvoicePrinting === true) {
+                    console.log('invoiceHandler::requestInvoiceForPickBox::skip invoice printing due to configuration');
+                    return;
+                }
+
+                if (config.has('printing.requestInvoiceDocumentsMerged') && config.get('printing.requestInvoiceDocumentsMerged') === true) {
+                    if (response.invoiceNumber === 'NO_INVOICE') {
+                        eventEmitter.emit('requestDocuments', {
+                            documentType: 'allDocs',
+                            invoiceNumber: response.shippingRequestNumber
+                        });
+                    } else {
+                        eventEmitter.emit('requestDocuments', {
+                            documentType: 'allDocs',
+                            invoiceNumber: response.invoiceNumber
+                        });
+                    }
+                } else {
+                    eventEmitter.emit('invoiceCreationSuccess', response);
+                }
+            })
+            .catch((res) => {
+                console.log('invoice creation/printing failed', res);
+
+                let event = 'pickBoxInvoiceFailed';
+                eventEmitter.emit(event, {...messageTemplate, event: event});
+            });
+    };
 }
 
 module.exports = InvoiceHandler;
